00808d36f5244ce728d1584b07f5a658d80fb535
kirillmorgunov <kirillmorgunov.mail@gmail.com>
2024-08-13 17:14:07 +0400

    [IN-13] add pagination

---
+++ src/components/ui/pagination/pagination.tsx
@@ -0,0 +1,103 @@
+import { ArrowIosBack, ArrowIosForward } from '@/assets/components'
+import { DOTS, usePagination } from '@/components/ui/pagination/hooks/usePagination'
+import clsx from 'clsx'
+
+import s from './pagination.module.scss'
+import { Button, Select, SelectItem } from '@/components/ui'
+import { cn } from '@/lib/utils'
+
+type Props = {
+  className?: string
+  currentPage: number
+  onPageChange: (page: number) => void
+  onPageSizeChange: (size: number) => void
+  pageSize: number
+  siblingCount?: number // колличество отображаемых эллементов между точек с каждой стороны от выбранной страницы
+  totalCount: number
+}
+export const Pagination = (props: Props) => {
+  let {
+    className,
+    currentPage,
+    onPageChange,
+    onPageSizeChange,
+    pageSize,
+    siblingCount = 1,
+    totalCount,
+    ...rest
+  } = props
+
+  const paginationRange = usePagination({
+    currentPage,
+    pageSize,
+    siblingCount,
+    totalCount,
+  })
+
+  // If there are less than 2 times in pagination range we shall not render the component
+  if (currentPage === 0 || paginationRange!.length < 2) {
+    return null
+  }
+  if (currentPage === null) {
+    currentPage = 1
+  }
+  const onNext = () => {
+    onPageChange(currentPage + 1)
+  }
+
+  const onPrevious = () => {
+    onPageChange(currentPage - 1)
+  }
+
+  const onPageSizeChangeHandler = (newValue: string) => {
+    onPageSizeChange(parseInt(newValue))
+  }
+
+  const lastPage = paginationRange![paginationRange!.length - 1]
+
+  return (
+    <div className={cn(s.wrapContainer, className)}>
+      <button className={cn(s.buttonLeft)} disabled={currentPage === 1} onClick={onPrevious}>
+        <ArrowIosBack />
+      </button>
+      <div className={cn(s.paginationContainer)}>
+        {paginationRange?.map((pageNumber, i) => {
+          // If the pageItem is a DOT, render the DOTS unicode character
+          if (pageNumber === DOTS) {
+            return (
+              <span className={cn(s.paginationItem, s.dots)} key={i}>
+                &#8230;
+              </span>
+            )
+          }
+
+          // Render our Page Pills
+          return (
+            <Button
+              className={cn(s.paginationItem, pageNumber === currentPage && s.selected)}
+              key={i}
+              onClick={() => onPageChange(+pageNumber)}
+            >
+              {pageNumber}
+            </Button>
+          )
+        })}
+      </div>
+      <button className={cn(s.buttonRight)} disabled={currentPage === lastPage} onClick={onNext}>
+        <ArrowIosForward />
+      </button>
+
+      <div className={s.selectContainer}>
+        Показать
+        <Select defaultValue={'10'} onValueChange={onPageSizeChangeHandler} {...rest}>
+          <SelectItem value={'10'}>10</SelectItem>
+          <SelectItem value={'20'}>20</SelectItem>
+          <SelectItem value={'30'}>30</SelectItem>
+          <SelectItem value={'50'}>50</SelectItem>
+          <SelectItem value={'100'}>100</SelectItem>
+        </Select>
+        на странице
+      </div>
+    </div>
+  )
+}
