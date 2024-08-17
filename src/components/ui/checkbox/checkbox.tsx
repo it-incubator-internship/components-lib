446cfd4b7b5528c7f98d2c180d776af3aaeca988
kirillmorgunov <kirillmorgunov.mail@gmail.com>
2024-08-17 14:59:09 +0400

    IN-2 fix

--- src/components/ui/checkbox/checkbox.tsx
+++ src/components/ui/checkbox/checkbox.tsx
@@ -1,4 +1,4 @@
-import {ComponentPropsWithoutRef, ElementRef, forwardRef} from 'react'
+import { ComponentPropsWithoutRef, ElementRef, forwardRef } from 'react'
 import * as RadixCheckbox from '@radix-ui/react-checkbox'
 import * as RadixLabel from '@radix-ui/react-label'
 import CheckmarkOutline from '../../../assets/components/CheckmarkOutline'
@@ -7,57 +7,58 @@ import clsx from 'clsx'
 
 type RadixCheckboxProps = typeof RadixCheckbox.Root
 type Props = {
-    checked?: boolean
-    labelText: string
-    disabled?: boolean
+  id?: string
+  checked?: boolean
+  className?: string
+  labelText: string
+  disabled?: boolean
 } & ComponentPropsWithoutRef<RadixCheckboxProps>
 
 export const Checkbox = forwardRef<ElementRef<RadixCheckboxProps>, Props>(
-    ({checked, labelText, disabled, ...rest}, ref) => {
-        const id = `id-${Date.now()}`;
+  ({ id, checked, labelText, className, disabled, ...rest }, ref) => {
+    const finalId = id ? id : `id-${Date.now()}`
 
-        let shadow = s.shadowEnabled;
-        let noHover = undefined;
-        let noFocus = undefined;
-        let labelDisabled = undefined;
+    let shadow = s.shadowEnabled
+    let noHover = undefined
+    let noFocus = undefined
+    let labelDisabled = undefined
 
-        switch (true) {
-            case disabled:
-                shadow = undefined;
-                noHover = s.checkboxShadowNoHover;
-                noFocus = s.checkboxShadowNoFocus;
-                labelDisabled = s.checkboxLabelDisabled;
-                break;
-            case checked:
-                shadow = s.shadowEnabled;
-                break;
-            case !checked:
-                shadow = undefined;
-                break;
-            default:
-                shadow = undefined;
-        }
-        return (
-            <div className={clsx(s.checkboxContainer)}>
-                <div className={clsx(s.checkboxShadow, shadow, noHover, noFocus)}
-                >
-                    <RadixCheckbox.Root
-                        id={id}
-                        className={clsx(s.checkboxRoot, checked ? '' : s.checkboxUnchecked)}
-                        checked={checked}
-                        ref={ref}
-                        disabled={disabled}
-                        onCheckedChange={rest.onCheckedChange}
-                    >
-                        <RadixCheckbox.Indicator className={clsx(s.checkboxIndicator)}>
-                            <CheckmarkOutline/>
-                        </RadixCheckbox.Indicator>
-                    </RadixCheckbox.Root>
-                </div>
-                <RadixLabel.Root htmlFor={id} className={clsx(s.checkboxLabel, labelDisabled)}>
-                    {labelText}
-                </RadixLabel.Root>
-            </div>
-        )
+    switch (true) {
+      case disabled:
+        shadow = undefined
+        noHover = s.checkboxShadowNoHover
+        noFocus = s.checkboxShadowNoFocus
+        labelDisabled = s.checkboxLabelDisabled
+        break
+      case checked:
+        shadow = s.shadowEnabled
+        break
+      case !checked:
+        shadow = undefined
+        break
+      default:
+        shadow = undefined
     }
+    return (
+      <div className={clsx(s.checkboxContainer)}>
+        <div className={clsx(s.checkboxShadow, shadow, noHover, noFocus)}>
+          <RadixCheckbox.Root
+            id={finalId}
+            className={clsx(s.checkboxRoot, checked ? '' : s.checkboxUnchecked)}
+            checked={checked}
+            ref={ref}
+            disabled={disabled}
+            onCheckedChange={rest.onCheckedChange}
+          >
+            <RadixCheckbox.Indicator className={clsx(s.checkboxIndicator)}>
+              <CheckmarkOutline />
+            </RadixCheckbox.Indicator>
+          </RadixCheckbox.Root>
+        </div>
+        <RadixLabel.Root htmlFor={finalId} className={clsx(s.checkboxLabel, labelDisabled)}>
+          {labelText}
+        </RadixLabel.Root>
+      </div>
+    )
+  }
 )
