8e6cc4859ed05b1ff19b919ed820dd35968a1674
Denis Krestov <rupo@list.ru>
2024-08-23 15:15:40 +0300

    [IN-30] refactor: update stories

--- src/components/ui/controlled/form-radio-group/form-radio-group.stories.tsx
+++ src/components/ui/controlled/form-radio-group/form-radio-group.stories.tsx
@@ -6,15 +6,6 @@ import { Button } from '../../button/button'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { z } from 'zod'
 
-const meta = {
-  component: FormRadioGroup,
-  title: 'Controlled/FormRadioGroup',
-} satisfies Meta<typeof FormRadioGroup>
-
-export default meta
-
-type Story = StoryObj<typeof meta>
-
 const userGradeOptions = [
   { label: 'Pre-junior', value: 'pre-junior' },
   { label: 'Junior', value: 'junior' },
@@ -25,59 +16,48 @@ const FormSchema = z.object({
   userGrade: z.string({ message: 'A variant must be selected' }),
 })
 
-export const ClearSelect: Story = {
-  name: 'Without default value',
+const FakeForm = ({ name, options, defaultValue }: { name: string, options: Array<typeof userGradeOptions>, defaultValue?: string }) => {
+  const { control, handleSubmit } = useForm({ resolver: zodResolver(FormSchema) })
+  const [data, setData] = useState<string>('')
+
+  const submitForm = handleSubmit(data => {
+    const option = userGradeOptions.find(option => option.label === data['userGrade'])
+    setData(option ? option.value : '')
+  })
+
+  return (
+    <>
+      <h2 style={{ margin: '10px 180px' }}>Form</h2>
+      <form onSubmit={submitForm} style={{ display: 'inline-flex', padding: '40px 80px', gap: '20px', border: '1px solid violet' }}>
+        <FormRadioGroup control={control} name={name} options={options} defaultValue={defaultValue} />
+        <Button variant={'outlined'}>Send selected</Button>
+      </form>
+      <h2 style={{ margin: '10px 10px' }}>{data && `Success! The value "${data}" has been sent.`}</h2>
+    </>
+  )
+}
+
+const meta = {
+  component: FakeForm,
+  title: 'Controlled/FormRadioGroup',
+} satisfies Meta<typeof FakeForm>
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+
+export const WithoutPreset = {
   args: {
     name: 'userGrade',
     options: userGradeOptions,
   },
-  render: (args) => {
-    const { control, handleSubmit } = useForm({ resolver: zodResolver(FormSchema) })
-    const [data, setData] = useState<string>('')
-
-    const submitForm = handleSubmit(data => {
-      const option = userGradeOptions.find(option => option.label === data['userGrade'])
-      setData(option ? option.value : '')
-    })
-
-    return (
-      <>
-        <h2 style={{ margin: '10px 180px' }}>Form</h2>
-        <form onSubmit={submitForm} style={{ display: 'inline-flex', padding: '40px 80px', gap: '20px', border: '1px solid violet' }}>
-          <FormRadioGroup {...args} control={control} />
-          <Button variant={'outlined'}>Send selected</Button>
-        </form>
-        <h2 style={{ margin: '10px 10px' }}>{data && `Success! The value "${data}" has been sent.`}</h2>
-      </>
-    )
-  },
 }
 
-export const PredSelect: Story = {
-  name: 'With default value',
+export const WithPreset = {
   args: {
     name: 'userGrade',
     options: userGradeOptions,
     defaultValue: 'Pre-junior',
-  },
-  render: (args) => {
-    const { control, handleSubmit } = useForm({ resolver: zodResolver(FormSchema) })
-    const [data, setData] = useState<string>('')
-
-    const submitForm = handleSubmit(data => {
-      const option = userGradeOptions.find(option => option.label === data['userGrade'])
-      setData(option ? option.value : '')
-    })
-
-    return (
-      <>
-        <h2 style={{ margin: '10px 180px' }}>Form</h2>
-        <form onSubmit={submitForm} style={{ display: 'inline-flex', padding: '40px 80px', gap: '20px', border: '1px solid violet' }}>
-          <FormRadioGroup {...args} control={control} />
-          <Button variant={'outlined'}>Send selected</Button>
-        </form>
-        <h2 style={{ margin: '10px 10px' }}>{data && `Success! The value "${data}" has been sent.`}</h2>
-      </>
-    )
   },
 }
