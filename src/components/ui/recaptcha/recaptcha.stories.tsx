df0de19a3acb553e257a0154560048b5e939d839
DirektPetr <direktpetr@ya.ru>
2024-08-29 13:20:16 +0300

    [IN-14] fix recaptcha 1

--- src/components/ui/recaptcha/recaptcha.stories.tsx
+++ src/components/ui/recaptcha/recaptcha.stories.tsx
@@ -1,43 +1,59 @@
 import { Meta, StoryObj } from '@storybook/react'
-import { Recaptcha } from './recaptcha'
+import { Recaptcha, RecaptchaProps } from './recaptcha'
+import { action } from '@storybook/addon-actions'
+import { useState } from 'react'
+
+type FakeRecaptcha = {
+  variant: RecaptchaProps['variant']
+  onClick?: () => void
+}
+
+const FakeRecaptcha = (props: FakeRecaptcha) => {
+  const { variant, onClick } = props
+
+  const [recaptchaVariant, setRecaptchaVariant] = useState(variant)
+
+  const handleOnClick = () => {
+    setRecaptchaVariant('loading')
+    setTimeout(() => {
+      setRecaptchaVariant('checked')
+    }, 1500)
+  }
+
+  return <Recaptcha variant={recaptchaVariant} onClick={onClick ?? handleOnClick} />
+}
 
 const meta = {
-  component: Recaptcha,
+  component: FakeRecaptcha,
   title: 'Components/Recaptcha',
-} satisfies Meta<typeof Recaptcha>
+} satisfies Meta<typeof FakeRecaptcha>
 
 export default meta
 
 type Story = StoryObj<typeof meta>
 
-export const Default: Story = {
-  args: { label: 'I’m not a robot' },
+export const Initial: Story = {
+  args: { variant: 'initial', onClick: action('Button clicked') },
 }
 
 export const Checked: Story = {
-  args: {
-    ...Default.args,
-  },
+  args: { variant: 'checked', onClick: action('Button clicked') },
 }
 
 export const Loading: Story = {
-  args: {
-    ...Default.args,
-  },
-}
-
-export const Error: Story = {
-  args: {
-    ...Default.args,
-    isError: true,
-    errorMsg: 'Please verify that you are not a robot',
-  },
+  args: { variant: 'loading', onClick: action('Button clicked') },
 }
 
 export const Expired: Story = {
+  args: { variant: 'expired', onClick: action('Button clicked') },
+}
+
+export const WithError: Story = {
+  args: { variant: 'withError', onClick: action('Button clicked') },
+}
+
+export const SuccessfulFlow: Story = {
   args: {
-    ...Default.args,
-    isExpired: true,
-    expiredMsg: 'Verifiction expired. Check the checkbox again.',
+    variant: 'initial',
   },
 }
--- src/components/ui/recaptcha/recaptcha.tsx
+++ src/components/ui/recaptcha/recaptcha.tsx
@@ -2,59 +2,74 @@ import { ComponentPropsWithoutRef } from 'react'
 import s from './recaptcha.module.scss'
 import { clsx } from 'clsx'
 
-import Recaptchalogo1 from '@/assets/components/Recaptchalogo1'
-import CheckmarkOutline from '@/assets/components/CheckmarkOutline'
+import RecycleIcon from '@/assets/components/Recaptchalogo1'
+import CheckIcon from '@/assets/components/CheckmarkOutline'
+
+type DisclaimerLinks = [string, string]
 
 export type RecaptchaProps = {
+  variant: 'initial' | 'checked' | 'loading' | 'withError' | 'expired'
+  disclaimerLinks?: DisclaimerLinks
   errorMsg?: string
-  isError?: boolean
   expiredMsg?: string
-  isExpired?: boolean
-  type?: 'default' | 'checked' | 'loading'
   label?: string
+  onClick?: () => void
 } & ComponentPropsWithoutRef<'div'>
 
-export const Recaptcha = ({
-  className,
-  label,
-  type = 'default',
-  isExpired,
-  expiredMsg,
-  isError,
-  errorMsg,
-  ...rest
-}: RecaptchaProps) => {
-  return (
-    <div {...rest} className={clsx(isError && s.error)}>
-      <div className={clsx(s.recaptchaRoot, className)}>
-        {isExpired && <div className={expiredMsg ? s.expiredMsg : ''}>{expiredMsg}</div>}
-        <div className={clsx(s[type], className)}>
-          <label className={s.containerLeft}>
-            {type === 'default' && (
-              <input className={s.checkbox} type="checkbox" name="checkbox" id="checkbox" />
-            )}
-            {type === 'checked' && (
-              <div className={s.checkedIcon}>
-                <CheckmarkOutline />
-              </div>
-            )}
-            {type === 'loading' && (
-              <div className={s.loadingIcon}>
-                <span className={s.loader}></span>
-              </div>
-            )}
-            {label && <div className={s.label}>{label}</div>}
-          </label>
+export const Recaptcha = (props: RecaptchaProps) => {
+  const {
+    variant,
+    disclaimerLinks = [
+      'https://policies.google.com/privacy?hl=ru',
+      'https://policies.google.com/terms?hl=ru',
+    ],
+    errorMsg = 'Please verify that you are not a robot',
+    expiredMsg = 'Verification expired. Check the checkbox again.',
+    label = 'I’m not a robot',
+    onClick,
+    ...rest
+  } = props
 
-          <div className={s.containerRight}>
-            <Recaptchalogo1 className={s.logo} />
-            <p className={s.privacy}>
-              <a href="#">Privacy</a> - Terms
-            </p>
-          </div>
+  return (
+    <div {...rest} className={variant === 'withError' ? s.errorWrapper : ''}>
+      <div className={clsx(s.recaptchaRoot)}>
+        <Indicator variant={variant} onClick={onClick} />
+        <div className={s.label}>{label}</div>
+        <div className={s.recaptchaDisclaimer}>
+          <RecycleIcon className={s.recycleIcon} />
+          <a href={disclaimerLinks[0]}>Privacy</a>
+          <span> - </span>
+          <a href={disclaimerLinks[1]}>Terms</a>
         </div>
+        {variant === 'expired' && <div className={s.expiredMsg}>{expiredMsg}</div>}
       </div>
-      {isError && <div className={s.errorMsg}>{errorMsg}</div>}
+      {variant === 'withError' && <div className={s.errorMsg}>{errorMsg}</div>}
     </div>
   )
+}
+
+type IndicatorProps = {
+  variant: RecaptchaProps['variant']
+  onClick?: () => void
+}
+
+const Indicator = ({ variant, onClick }: IndicatorProps) => {
+  function getIndicator(variant: RecaptchaProps['variant']) {
+    switch (true) {
+      case variant === 'initial' || variant === 'withError' || variant === 'expired':
+        return <button className={s.btnCheck} onClick={onClick}></button>
+      case variant === 'checked':
+        return (
+          <div className={s.checkedIcon}>
+            <CheckIcon />
+          </div>
+        )
+      case variant === 'loading':
+        return <div className={s.loader}></div>
+      default:
+        return <div></div>
+    }
+  }
+
+  return <>{getIndicator(variant)}</>
 }
