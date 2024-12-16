cf2024a6268359c4ff9429dc0e3c3fa8c7d0c6e4
Shevchenko Andrey <demorest49de@gmail.com>
2024-12-16 22:36:11 +0300

    Changes combobox.stories.tsx

--- src/components/ui/shadcn/combobox/combobox.stories.tsx
+++ src/components/ui/shadcn/combobox/combobox.stories.tsx
@@ -1,17 +1,24 @@
-import ComboBox from "./combobox";
-import {Meta, StoryObj} from "@storybook/react";
+import ComboBox from './combobox'
+import { Meta, StoryObj } from '@storybook/react'
 
 const meta = {
-    component: ComboBox,
-} satisfies Meta<typeof ComboBox>;
+  component: ComboBox,
+} satisfies Meta<typeof ComboBox>
 
-export default meta;
+export default meta
 
 type Story = StoryObj<typeof meta>
 
-export const Primary ={
-    args:{
-        options:  ["Apple", "Grapes", "Pineapple", "Grapefruit"],
-    }
+export const Primary = {
+  args: {
+    options: ['Apple', 'Grapes', 'Pineapple', 'Grapefruit'],
+  },
+  render: (args) => {
+    return (
+      <>
+        <ComboBox {...args} />
+        <h1>hello</h1>
+      </>
+    )
+  },
 } satisfies Story
-
