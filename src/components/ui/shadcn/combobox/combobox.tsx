e7a44ea1962539c7d656940b0a86de3de0b0c94c
Shevchenko Andrey <demorest49de@gmail.com>
2024-12-18 13:50:38 +0300

    Changes combobox.tsx package.json pnpm-lock.yaml

--- src/components/ui/shadcn/combobox/combobox.tsx
+++ src/components/ui/shadcn/combobox/combobox.tsx
@@ -1,9 +1,8 @@
-import React, {useState, KeyboardEvent, useEffect} from 'react'
+import React, { useState, KeyboardEvent, useEffect } from 'react'
 import { ComponentPropsWithoutRef } from 'react'
 
 import * as Popover from '@radix-ui/react-popover'
 import { cn } from '@/components/ui/shadcn/combobox/cn'
-import { Command, CommandEmpty, CommandGroup, CommandItem, CommandList } from 'cmdk'
 
 type ComboboxProps = ComponentPropsWithoutRef<typeof Popover.Root> & {
   variant?: 'primary' | 'secondary' | 'outlined' | 'text'
@@ -11,55 +10,70 @@ type ComboboxProps = ComponentPropsWithoutRef<typeof Popover.Root> & {
   options: string[]
 }
 
-/*
-e => {
-          if (e.key === 'ArrowDown') {
-            e.preventDefault()
-            setOpen(true)
-          }
-          if (e.key === 'Enter') {
-            e.preventDefault()
-          }
-        }
- */
-
 export default function ComboBox({ options }: ComboboxProps) {
-  const [inputValue, setInputValue] = useState('')
-  const [open, setOpen] = useState(false)
+  const [inputValue, setInputValue] = useState<string>('')
+  const [open, setOpen] = useState<boolean>(false)
 
-  const [selectedIndex, setSelectedIndex] = useState(-1)
+  const [selectedIndex, setSelectedIndex] = useState<number>(-1)
+  const [currentOptions, setCurrentOptions] = useState<string[]>(options)
 
-  useEffect(()=>{
+  useEffect(() => {
     if (selectedIndex >= 0) {
-      const selectedOption = options[selectedIndex]
+      const selectedOption = currentOptions[selectedIndex]
       if (selectedOption) {
         setInputValue(selectedOption)
       }
+    } else {
+      setInputValue('')
+      setOpen(false)
     }
-  },[selectedIndex])
+  }, [selectedIndex])
 
-  const filteredOptions = options.filter(item =>
-    item.toLowerCase().includes(inputValue.toLowerCase())
-  )
+  useEffect(() => {
+    if (!inputValue) {
+      setCurrentOptions(options)
+      setSelectedIndex(-1)
+    }
+  }, [inputValue])
 
+  const filterOptions = () => {
+    setCurrentOptions(options.filter(item => item.toLowerCase().includes(inputValue.toLowerCase())))
+    if (!inputValue) {
+      setSelectedIndex(-1)
+    }
+  }
+
+  console.log(' selectedIndex: ', selectedIndex)
   const handleKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
-
     if (e.key === 'ArrowDown') {
       e.preventDefault()
-      !open&& setOpen(true)
-      setSelectedIndex(prevIndex => Math.min(options.length - 1, prevIndex + 1))
+
+      if (!open) {
+        setOpen(true)
+      }
+
+      setSelectedIndex(prevIndex => {
+        if (prevIndex + 1 === currentOptions.length) {
+          return 0
+        }
+        return prevIndex + 1
+      })
     }
+
     if (e.key === 'ArrowUp') {
       e.preventDefault()
-      setSelectedIndex(prevIndex => Math.max(0, prevIndex - 1))
+      setSelectedIndex(prevIndex => {
+        return Math.max(-1, prevIndex - 1)
+      })
     }
-    if (e.key === 'Enter' && selectedIndex >= 0) {
+
+    if (e.key === 'Enter' && selectedIndex >= -1) {
       e.preventDefault()
-      const selectedOption = options[selectedIndex]
+      const selectedOption = currentOptions[selectedIndex]
       if (selectedOption) {
         setInputValue(selectedOption)
-        setOpen(false)
       }
+      setOpen(prevValue => !prevValue)
     }
   }
 
@@ -71,14 +85,11 @@ export default function ComboBox({ options }: ComboboxProps) {
           placeholder="Select an option..."
           value={inputValue}
           onChange={e => {
-          //.filter(item => item.toLowerCase().includes(inputValue.toLowerCase()))
-            setInputValue(e.target.value)
+            setInputValue(e.currentTarget.value)
             setOpen(true)
-
-            setSelectedIndex(-1)
+            filterOptions()
           }}
           onKeyDown={handleKeyDown}
-          autoFocus
           className={cn(
             'w-[200px] p-2 rounded cursor-text border-[1px] border-solid border-[#ccc]'
           )}
@@ -94,47 +105,31 @@ export default function ComboBox({ options }: ComboboxProps) {
           )}
           onOpenAutoFocus={e => e.preventDefault()}
         >
-          <Command
-            loop
-            onValueChange={value => {
-              setInputValue(value)
-              setOpen(false)
-            }}
-          >
-            <CommandList>
-              {options?.length > 0 ? (
-                <CommandGroup>
-                  {options
-                    .map(item => (
-                      <CommandItem
-                        key={item}
-                        value={item}
-                        onSelect={() => {
-                          setInputValue(item)
-                          setOpen(false)
-                        }}
-                        onClick={() => {
-                          setInputValue(item)
-                          setOpen(false)
-                        }}
-
-                        style={{
-                          padding: '8px',
-                          cursor: 'pointer',
-                          borderBottom: '1px solid #f0f0f0',
-                        }}
-                      >
-                        {item}
-                      </CommandItem>
-                    ))}
-                </CommandGroup>
-              ) : (
-                <CommandEmpty>
-                  <div style={{ padding: '8px', color: '#999' }}>No options found</div>
-                </CommandEmpty>
-              )}
-            </CommandList>
-          </Command>
+          {currentOptions?.length > 0 ? (
+            currentOptions.map((item, index) => (
+              <div
+                key={item}
+                onSelect={() => {
+                  setInputValue(item)
+                  setOpen(false)
+                }}
+                onClick={() => {
+                  setInputValue(item)
+                  setOpen(false)
+                  setSelectedIndex(index)
+                }}
+                style={{
+                  padding: '8px',
+                  cursor: 'pointer',
+                  borderBottom: '1px solid #f0f0f0',
+                }}
+              >
+                {item}
+              </div>
+            ))
+          ) : (
+            <div style={{ padding: '8px', color: '#999' }}>No options found</div>
+          )}
         </Popover.Content>
       </Popover.Portal>
     </Popover.Root>
